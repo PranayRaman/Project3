0a3bb01fe8aa8bac16cfc8f50fa4a3c6eb5c4747
John Gionti <jgionti@ufl.edu>
2021-04-05 10:22:54 -0400

    pushed Game class prototype

---
+++ Game.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+// Basic data container for games
+// Main identifier for trees/maps will be id
+struct Game {
+    int id = 0;
+    string name = "";
+    vector<string> developers;
+    vector<string> genres;
+    string releaseDate = "";
+    double rating = 0.0;
+    int playtime = 0;
+    int achievementCount = 0;
+};
+
+/*
+Game class stored as:
+	id (tree/map are constructed with this as node/index)
+	name
+	developer(s)
+	genre (replace blank with ?)
+	date released
+	metacritic rating (repl. 0 with N/A when displaying)
+	playtime (repl. 0 with ? when displaying)
+	achievements (repl. 0 with ? when displaying)
+*/
