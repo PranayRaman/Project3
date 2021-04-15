3f2094b856ea331d6302badaa36691dae6a705f0
nathankrasno <nakrasno@gmail.com>
2021-04-15 18:51:12 -0400

    main update

--- main.cpp
+++ main.cpp
@@ -4,15 +4,9 @@
 #include "Timer.h"
 
 int main() {
-<<<<<<< HEAD
    
     return -0;
-=======
     vector<Game> g;
     Load("game_info.csv", g);
-    for(int i = 0; i < g.size(); i++){
-        g[i].printInfo();
-    }
     return 0;
->>>>>>> af42a834e843f02c7d500c786f1c376f44762f97
 }
