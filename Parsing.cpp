56dd67eee0f6dba88928ee17eaa8b9e7d53302c5
nathankrasno <nakrasno@gmail.com>
2021-04-15 18:35:37 -0400

    parsing class

---
+++ Parsing.cpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+class Parsing{ //john do you want to search by all of these?
+//or only some. They each would need their own AVL tree to build
+    string KnowYourGame = 
+    "#################\n"
+    "# Know Your Game #\n"
+    "##################";
+    string question1 = "How would you like to get to Know Your Game?";
+    string q1options = 
+    "1. Search by ID\n"
+    "2. Search by name\n";
+    
+};
--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include "FileRead.cpp";
 
 int main() {
-    vector<Game> g;
-    Load("game_info.csv", g);
-    for(int i = 0; i < g.size(); i++){
-        g[i].printInfo();
-    }
-    return -32466854;
+   
+    return -0;
 }
