5abc3a728b11f85f84e39ec15ed1b226202e6517
nathankrasno <nakrasno@gmail.com>
2021-04-18 19:16:11 -0400

    added line and blankline

--- Parsing.h
+++ Parsing.h
@@ -1,19 +1,16 @@
 #include <string>
+#include "Game.h"
 using namespace std;
 class Parsing{ //john do you want to search by all of these?
 //or only some. They each would need their own AVL tree to build
-    string KnowYourGame = 
-    "#################\n"
-    "# Know Your Game #\n"
-    "##################";
-    string question1 = "How would you like to get to Know Your Game?";
-    string q1options = 
-    "1. Search by ID\n"
-    "2. Search by name\n";
 public:
+    string line = "|-----------------------------------------------------------------------------|\n";
+    string blankline = "|                                                                             |\n";
+    
     void printSpaces(int n){
         for(int i = 0; i < n; i++){
             cout << " ";
         }
     }
+
 };
